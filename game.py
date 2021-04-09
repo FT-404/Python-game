752e382c7a832901f190c7e38f96c8aaf94eac20
Yaroslav <yarpav5646@gmail.com>
2021-04-09 19:10:10 +0500

    Beta 1.0

--- game.py
+++ game.py
@@ -1,41 +1,49 @@
 from tkinter import *
 from time import *
 from random import randint
+from PIL import Image, ImageTk
 
 root = Tk()
+image = PhotoImage(file="pt.png")
 root.title('Прямоугольник зеленый')
-root.geometry('920x480')
+root.geometry('1920x1080')
 root.configure(background='#fff')
-holst = Canvas(root, bg='#fff', width=1920, height=1080, highlightbackground="#fff")
-lab = Label(bg='#fff', font="20")
-lab.pack()
-gameTimerLab = Label(bg='#fff', font='40')
-gameTimerLab.pack()
-counter = Label(bg='#fff', font="20")
-counter.pack()
+holst = Label(root, width=1920, height=1080, highlightbackground="#fff")
+img = Image.open("bg2.jpg")
+holst.img = ImageTk.PhotoImage(img)
+holst['image'] = holst.img
+lab = Label(bg='#6797ec', font="20")
+count = 0
+gameTimerLab = Label(bg='#6797ec', font='40')
+counter = Label(bg='#6797ec', font="20")
 
 
 def Starter():
+    lab.place(x=950, y=20)
+    counter.place_forget()
     global backTimer
     backTimer = 4
-    gameTimer = 10
-    btnRec.pack_forget()
-    btnMatch.pack_forget()
-
+    gameTimer = 30
+    btnRec.place_forget()
+    btnMatch.place_forget()
     for x in range(4):
         backTimer -= 1
         if x == 3:
             backTimer = 'Start!'
         lab.config(text=str(backTimer))
         sleep(1)
+
         root.update()
     if backTimer == 'Start!':
         sleep(1)
+        counter.place(x=950, y=70)
+        gameTimerLab.place(x=930, y=20)
         timing = time()
-        lab.destroy()
+        backTimer = ''
+        lab.config(text=str(backTimer))
         target.place(x=koordX(), y=koordY())
         root.update()
-        counter.config(text='Счёт: ' + str(0))
+        counter.config(text='Счёт: ' + str(count))
         while gameTimer != 0:
             if time() - timing > 1:
                 timing = time()
@@ -45,18 +53,20 @@ def Starter():
             root.update()
 
         if gameTimer == 0:
-            gameTimerLab.destroy()
-            target.destroy()
-            btnRec.pack()
-            btnMatch.pack()
-            if count == 1:
+            target.place_forget()
+
+            if count % 10 == 1:
                 ans = ' очко!'
-            elif count >= 2 and count <= 4:
+            elif count % 10 >= 2 and count % 10 <= 4:
                 ans = ' очка!'
             else:
                 ans = ' очков!'
             counter.config(text='Вы набрали - ' + str(count) + ans)
-
+            counter.place(x=920, y=70)
+            gameTimer = ''
+            gameTimerLab.config(text=gameTimer)
+            btnRec.place(x=847.5, y=260)
+            btnMatch.place(x=847.5, y=300)
 
 
 def koordX():
@@ -69,18 +79,21 @@ def koordY():
     return y
 
 
-def RecruitMod():
-    Starter()
-
-
-count = 0
-
-
 def onTargetClick():
     global count
     count += 1
     counter.config(text='Счёт: ' + str(count))
     target.place(x=koordX(), y=koordY())
+
+
+def RecruitMod():
+    Starter()
+
+
+def MatchMod():
+    global count
+    count = 0
+    Starter()
 
 
 btnRec = Button(
@@ -93,7 +106,7 @@ btnRec = Button(
     width=20,
     command=RecruitMod
 )
-btnRec.pack()
+btnRec.place(x=847.5, y=260)
 btnMatch = Button(
     text='Режим "Продвинутый"',
     background="#555",
@@ -101,17 +114,17 @@ btnMatch = Button(
     padx="20",
     pady="8",
     font="20",
-    width=20
+    width=20,
+    command=MatchMod
 )
 
-btnMatch.pack()
-image = PhotoImage(file="pt.png")
+btnMatch.place(x=847.5, y=300)
+
 target = Button(
-    background="#fff",
+    background="#6797ec",
     foreground="#fff",
     image=image,
-    activebackground="#fff",
-    highlightcolor="#fff",
+    activebackground="#6797ec",
     bd=0,
     command=onTargetClick
 )
