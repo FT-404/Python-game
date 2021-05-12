6041094c4ee28c50744b7affd3adffeb3204e37f
Yaroslav <yarpav5646@gmail.com>
2021-05-12 21:42:40 +0500

    Realise

--- game.py
+++ game.py
@@ -1,26 +1,31 @@
 from tkinter import *
 from time import *
 from random import *
-from PIL import Image, ImageTk
 
 root = Tk()
 image = PhotoImage(file="bird2.png")
 image2 = PhotoImage(file="anim.png")
 bg = PhotoImage(file="bg.png")
-root.title('Прямоугольник зеленый')
+root.title('Игра про птичек')
 root.geometry('1920x1080')
 root.configure(background='#fff')
 root.config(cursor='tcross')
 holst = Label(root, width=1920, height=1080, highlightbackground="#fff", image=bg)
+
 # img = Image.open("bg2.jpg")
 # holst.img = ImageTk.PhotoImage(img)
 # holst['image'] = holst.img
+
 lab = Label(bg='#6797ec', font="20")
-count = 0
 gameTimerLab = Label(bg='#6797ec', font='40')
 counter = Label(bg='#6797ec', font="20")
-lives = 1
+
+lives = 3
 flag = 0
+count = 0
+bossCount = 0
+ff = 0
+bosser = 1
 
 
 def Starter():
@@ -62,8 +67,8 @@ def Starter():
         btnMatch.place(x=847.5, y=300)
 
 
-def koordX():
-    x = uniform(0.25, 0.75)
+def koordX(first, second):
+    x = uniform(first, second)
     return x
 
 
@@ -73,37 +78,59 @@ def koordY():
 
 
 def onTargetClick():
-    global lives, flag, count, image
-    if flag == 0:
-        count += 1
-        counter.config(text='Счёт: ' + str(count))
-    x = koordX()
+    global lives, flag, count, image, image2, ff, bosser
+
     y = koordY()
-    k = 50
-    for i in range(1920):
-        target.place(x=i / x, y=y)
-        if int(target.place_info().get('x')) > k and int(target.place_info().get('x')) < k + 50:
-            target.config(image=image2)
+    if ff == 10 * bosser - 1:
 
-        elif int(target.place_info().get('x')) > k + 50 and int(target.place_info().get('x')) < k + 100:
-            target.config(image=image)
-            k += 100
+        image = PhotoImage(file="i.png")
+        image2 = PhotoImage(file="i.png")
+        ff += 1
+        bosser += 1
 
-        if int(target.place_info().get('x')) >= 1920:
-            lives -= 1
-            flag = 1
+        onTargetClick()
+    else:
+        x = koordX(0.25, 0.75)
+        print('bosser:', 10 * (bosser - 1) - 1, '\n count: ', count)
 
-            onTargetClick()
-        else:
-            flag = 0
-        root.update()
-        if lives <= 0:
-            target.place_forget()
-            break
+        if count == 10 * (bosser - 1) - 1 and count != -1:
+
+            x = koordX(0.1, 0.18)
+
+        if count == ff:
+            image = PhotoImage(file="bird2.png")
+            image2 = PhotoImage(file="anim.png")
+        if flag == 0:
+            count += 1
+            ff = count
+            counter.config(text='Счёт: ' + str(count))
+
+        k = 50
+
+        for i in range(1920):
+            target.place(x=i / x, y=y)
+            if int(target.place_info().get('x')) > k and int(target.place_info().get('x')) < k + 50:
+                target.config(image=image2)
+
+            elif int(target.place_info().get('x')) > k + 50 and int(target.place_info().get('x')) < k + 100:
+                target.config(image=image)
+                k += 100
+
+            if int(target.place_info().get('x')) >= 1920:
+                lives -= 1
+                flag = 1
+
+                onTargetClick()
+            else:
+                flag = 0
+            root.update()
+            if lives <= 0:
+                target.place_forget()
+                break
 
 
 def RecruitMod():
-    global count
+    global count, lives
     count -= 1
     Starter()
 
@@ -115,7 +142,7 @@ def MatchMod():
 
 
 btnRec = Button(
-    text='Режим "Новичок"',
+    text='Режим "Тренировка"',
     background="#555",
     foreground="#ccc",
     padx="20",
@@ -126,7 +153,7 @@ btnRec = Button(
 )
 btnRec.place(x=847.5, y=260)
 btnMatch = Button(
-    text='Режим "Продвинутый"',
+    text='Режим "Соревновательный"',
     background="#555",
     foreground="#ccc",
     padx="20",
@@ -146,5 +173,5 @@ target = Button(
     bd=0,
     command=onTargetClick
 )
-holst.pack()
+holst.place(x=0, y=0)
 holst.mainloop()
